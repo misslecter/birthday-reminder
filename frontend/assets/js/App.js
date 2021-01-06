e1827dc2cf44125805891540e70e56890646a5c4
anastasia <anastasia.surikova@akka.eu>
2021-01-06 19:10:52 +0100

    fb api

--- frontend/assets/js/App.js
+++ frontend/assets/js/App.js
@@ -1,9 +1,60 @@
 export class App {
+
+  apiKey;
+  userID;
+
+  appId = 400741717660932;
+  appSecret = 'd9d88702b63054870e4f1cd8c3283309';
+  redirectUrl = 'https://localhost:1234/';
+
   async init() {
 
     console.log('Init');
 
     this.initFacebookLogin();
+    this.initLoginState();
+  }
+
+  initLoginState() {
+    window.checkLoginState = function () {
+      FB.getLoginStatus(function (response) {
+        console.log(response, this);
+        this.apiKey = response.authResponse.accessToken;
+        this.userID = response.authResponse.userID;
+        this.getFbProfile();
+      }.bind(this));
+    }.bind(this);
+  }
+
+  getFbProfile() {
+    FB.api(
+      '/me/',
+      'GET',
+      {},
+      function (response) {
+        console.log(response);
+        // Insert your code here
+
+        const container = document.querySelector('[data-js-selector=\'user-name\']');
+        if (response.name && container) {
+          container.innerHTML = `Hello, ${response.name}`;
+        }
+      }
+    );
+    FB.api(
+      '/me/friends',
+      'GET',
+      {},
+      function (response) {
+        console.log(response);
+        // Insert your code here
+
+        const container = document.querySelector('[data-js-selector=\'user-friends\']');
+        if (response.summary && response.summary.total_count && container) {
+          container.innerHTML = `You have total of ${response.summary.total_count} friends on Facebook`;
+        }
+      }
+    );
   }
 
   initFacebookLogin() {
@@ -16,7 +67,6 @@ export class App {
       });
 
       FB.AppEvents.logPageView();
-
     };
 
     (function (d, s, id) {
