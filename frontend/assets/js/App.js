7e608cfc718d42df7becdffc48f5efd11958c1ca
anastasia <anastasia.surikova@akka.eu>
2021-01-06 14:48:15 +0100

    fb login button

--- frontend/assets/js/App.js
+++ frontend/assets/js/App.js
@@ -2,5 +2,32 @@ export class App {
   async init() {
 
     console.log('Init');
+
+    this.initFacebookLogin();
+  }
+
+  initFacebookLogin() {
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: '307250724061619',
+        cookie: true,
+        xfbml: true,
+        version: 'v0.1'
+      });
+
+      FB.AppEvents.logPageView();
+
+    };
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 }
